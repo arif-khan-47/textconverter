5bd7ae202870f19c87bb66618978fd1f7d3ac96f
Arif <aarifkhan873287@gmail.com>
2022-12-06 08:32:20 +0530

    update

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert(props){
   return (
-    props.alert && <div className='bg-blue-400 border-blue-700 border p-5 font-semibold text-xl' role="alert">
+    props.alert && <div className='bg-blue-400 border-blue-700 border p-5 font-semibold text-xl text-white' role="alert">
     {props.alert.msg}
   </div>
   )
